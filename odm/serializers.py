88be38b42f5e34dc8760a75a1a76dd5420291974
brunoryamada <bryamada@gmail.com>
2018-07-16 17:54:17 -0300

    fix odm serializer

--- odm/serializers.py
+++ odm/serializers.py
@@ -1,11 +1,13 @@
 import json
 from datetime import datetime
 
-from bson import ObjectId
+from bson import ObjectId, Timestamp
 
 
 class ODMSerializer(json.JSONEncoder):
     def default(self, o):
+        if isinstance(o, Timestamp):
+            return o.as_datetime().isoformat() + 'Z'
         if isinstance(o, ObjectId):
             return str(o)
         if isinstance(o, datetime):
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odm',
-    version='0.0.4',
+    version='0.0.5',
     description='Engine MongoDB',
     author='Lucas Ribeiro',
     author_email='lucas.ribeiro@gruponewway.com.br',
