9a2f1f66d1732ee667dc5af13767a2e3fa2eb6d0
brunoryamada <bryamada@gmail.com>
2019-01-22 22:36:02 -0200

    version upgrade - v0.1.5 => v0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odm',
-    version='0.1.5',
+    version='0.1.6',
     description='Engine MongoDB',
     author='Grupo New Way',
     author_email='contato@gruponewway.com.br',
