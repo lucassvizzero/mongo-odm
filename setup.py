a59c34852312b11d73e2ca40079a30ff64b61cbb
brunoryamada <bryamada@gmail.com>
2019-01-24 09:46:22 -0200

    Version v0.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odm',
-    version='0.1.6',
+    version='0.1.7',
     description='Engine MongoDB',
     author='Grupo New Way',
     author_email='contato@gruponewway.com.br',
