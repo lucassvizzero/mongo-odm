a6bfb7e5e107eddc40bcba2f098b9333de1b9ab7
brunoryamada <bryamada@gmail.com>
2019-02-13 17:30:23 -0200

    version bump v0.2.8 => v0.2.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odm',
-    version='0.2.8',
+    version='0.2.9',
     description='Engine MongoDB',
     author='Grupo New Way',
     author_email='contato@gruponewway.com.br',
