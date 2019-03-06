ddec1ea958071f621c7875d9e2159f3fb92c0d1e
brunoryamada <bryamada@gmail.com>
2019-03-06 09:28:21 -0300

    update dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odm',
-    version='0.2.10',
+    version='0.2.11',
     description='Engine MongoDB',
     author='Grupo New Way',
     author_email='contato@gruponewway.com.br',
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     url='https://git.newwaycorp.io/libraries/python/mongo-odm',
     install_requires=[
-        'pymongo>=3.7.0',
+        'motor==1.3.1',
         'jsonschema>=2.6.0'
     ]
 )
